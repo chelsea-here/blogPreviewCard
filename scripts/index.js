11c08b562223ff907d295ee2f68b393e42a0f0d5
Chelsea Cruz <125297600+chelsea-here@users.noreply.github.com>
2024-06-26 15:27:15 -0500

    added javascript hover effect

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,11 @@
-// addEventListener("mouseover", (event) => {});
+// selecting the element
+const title = document.getElementById('title');
 
-// onmouseover = (event) => {};
+// accessing the color variables from my css file
+const style = getComputedStyle(document.body);
+const yellow = style.getPropertyValue('--primary-color-yellow');
+
+// creating mouseover effect
+title.addEventListener("mouseover", function (e) {
+  this.style.color = yellow;
+});
