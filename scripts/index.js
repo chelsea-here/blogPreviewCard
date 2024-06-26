1f2382ef1cc85e7e85a4038d1697c6f5d4c8c360
Chelsea Cruz <125297600+chelsea-here@users.noreply.github.com>
2024-06-26 15:43:28 -0500

    added mouseout event for title, added comments explaining code

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,16 @@
 // selecting the element
-const title = document.getElementById('title');
+const title = document.getElementById("title");
 
 // accessing the color variables from my css file
 const style = getComputedStyle(document.body);
-const yellow = style.getPropertyValue('--primary-color-yellow');
+const yellow = style.getPropertyValue("--primary-color-yellow");
 
 // creating mouseover effect
 title.addEventListener("mouseover", function (e) {
   this.style.color = yellow;
+});
+
+// now i need a mouseout event listener so its only active during my hover
+title.addEventListener("mouseout", function (e) {
+  this.style.color = "";
 });
